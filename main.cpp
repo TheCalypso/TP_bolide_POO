9d6e0c118698a42d841bee2a2ca1b7e73e252031
TheCalypso <bastien.dorangeon.penichot@gmail.com>
2024-03-28 11:09:39 +0100

    feat: create class Buzzer in main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
+class Buzzer {
+    public:
+        Buzzer(PinName br_buzzer):
+            _buzzer(br_buzzer){
+                _buzzer.period_ms(1);
+                _buzzer.write(0.0);
+            }
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+        void start(){
+            _buzzer.write(0.5);
+        }
 
+        void stop(){
+            _buzzer.write(0.0);
+        }
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
+    private:
+        PwmOut _buzzer;
+};
+
+Buzzer mybuzzer(A1);
+
+/* CONST */
+#define WAIT_2_MS 2
+#define WAIT_100_MS 100
+#define WAIT_2000_MS 2000
+
+int main(){
+    printf("Hello world\n");
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        mybuzzer.start();
+        thread_sleep_for(WAIT_100_MS);
+
+        mybuzzer.stop();
+        thread_sleep_for(WAIT_2000_MS);
+
+        thread_sleep_for(WAIT_2_MS);
     }
 }
